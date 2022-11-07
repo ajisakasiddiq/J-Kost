6d1d181c97dcd38a5f632c9684d2bbac0335572e
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2022-11-07 10:08:54 +0700

    Create function.php

---
+++ function.php
@@ -0,0 +1,16 @@
+<?php  
+$conn = mysqli_connect("localhost","root","","db_kos");
+
+function query($query){
+    global $conn;
+    $result = mysqli_query($conn,$query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)){
+        $rows[]= $row;
+
+    }
+
+    return $rows;
+}
+
+?>
