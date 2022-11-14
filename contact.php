ec5f6c5d487c0021d85a420f580758a116ef2c20
kiranaard <117187964+kiranaard@users.noreply.github.com>
2022-11-14 09:52:41 +0700

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,270 +1,85 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="utf-8">
-    <title>J-KOS</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="Free Website Template" name="keywords">
-    <meta content="Free Website Template" name="description">
+if(!$_POST) exit;
 
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
+// Email address verification, do not edit.
+function isEmail($email) {
+	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
+}
 
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
+$position_post  = $_POST['position_post'] 
+$first_name     = $_POST['first_name'];
+$last_name     = $_POST['last_name'];
+$email    = $_POST['email'];
+$comments = $_POST['comments'];
 
-    <!-- CSS Libraries -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+if(trim($first_name) == '') {
+	echo '<div class="error_message">Attention! You must enter your name.</div>';
+	exit();
+}  else if(trim($email) == '') {
+	echo '<div class="error_message">Attention! Please enter a valid email address.</div>';
+	exit();
+} else if(!isEmail($email)) {
+	echo '<div class="error_message">Attention! You have enter an invalid e-mail address, try again.</div>';
+	exit();
+}
 
-    <!-- Template Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
-</head>
+if(trim($comments) == '') {
+	echo '<div class="error_message">Attention! Please enter your message.</div>';
+	exit();
+}
 
-<body>
-    <!-- Top Bar Start -->
-    <div class="top-bar">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-4 col-md-3">
-                    <div class="logo">
-                        <a href="index.html">
-                            <h1>J<span>KOS</span></h1>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-4 col-md-4">
-                    <div class="top-bar-item">
-                        <div class="top-bar-icon">
-                            <i class="fa fa-phone-alt"></i>
-                        </div>
-                        <div class="top-bar-text">
-                            <h3>Call Us</h3>
-                            <p>0891 3687 7252</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4 col-md-4">
-                    <div class="top-bar-item">
-                        <div class="top-bar-icon">
-                            <i class="far fa-envelope"></i>
-                        </div>
-                        <div class="top-bar-text">
-                            <h3>Email Us</h3>
-                            <p>zidanprasetyo@gmail.com</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-    </div>
-    <!-- Top Bar End -->
-
-    <!-- Nav Bar Start -->
-    <div class="nav-bar">
-        <div class="container">
-            <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-                <a href="#" class="navbar-brand">MENU</a>
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                    <div class="navbar-nav mr-auto">
-                        <a href="index.php" class="nav-item nav-link ">Home</a>
-                        <a href="about.php" class="nav-item nav-link">Tentang Kami</a>
-                        <a href="service.php" class="nav-item nav-link">Pelayanan</a>
-                        <a href="order.php" class="nav-item nav-link">Pemesanan</a>
-                        <a href="contact.php" class="nav-item nav-link active">Contact</a>
-                    </div>
-                    <div class="ml-auto">
-                        <a href="register.php" class="btn-regis ">Register</a>
-                        <a href="login.php" class="btn btn-custom">Login</a>
-                    </div>
-
-                </div>
-            </nav>
-        </div>
-    </div>
-    <!-- Nav Bar End -->
+if(get_magic_quotes_gpc()) {
+	$comments = stripslashes($comments);
+}
 
 
-    <!-- Page Header Start -->
-    <div class="page-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h2>Contact Us</h2>
-                </div>
-                <div class="col-12">
-                    <a href="">Home</a>
-                    <a href="">Contact</a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Page Header End -->
+// Configuration option.
+// Enter the email address that you want to emails to be sent to.
+// Example $address = "joe.doe@yourdomain.com";
+
+//$address = "example@themeforest.net";
+$address = "example@yourdomain.com";
 
 
-    <!-- Contact Start -->
-    <div class="contact">
-        <div class="container">
-            <div class="section-header text-center">
-                <p>Get In Touch</p>
-                <h2>Contact for any query</h2>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="contact-info">
-                        <h2>Quick Contact Info</h2>
-                        <div class="contact-info-item">
-                            <div class="contact-info-icon">
-                                <i class="far fa-clock"></i>
-                            </div>
-                            <div class="contact-info-text">
-                                <h3>Opening Hour</h3>
-                                <p>Mon - Fri, 8:00 - 9:00</p>
-                            </div>
-                        </div>
-                        <div class="contact-info-item">
-                            <div class="contact-info-icon">
-                                <i class="fa fa-phone-alt"></i>
-                            </div>
-                            <div class="contact-info-text">
-                                <h3>Call Us</h3>
-                                <p>+012 345 6789</p>
-                            </div>
-                        </div>
-                        <div class="contact-info-item">
-                            <div class="contact-info-icon">
-                                <i class="far fa-envelope"></i>
-                            </div>
-                            <div class="contact-info-text">
-                                <h3>Email Us</h3>
-                                <p>info@example.com</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-7">
-                    <div class="contact-form">
-                        <div id="success"></div>
-                        <form name="sentMessage" id="contactForm" novalidate="novalidate">
-                            <div class="control-group">
-                                <input type="text" class="form-control" id="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
-                                <p class="help-block text-danger"></p>
-                            </div>
-                            <div class="control-group">
-                                <input type="email" class="form-control" id="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
-                                <p class="help-block text-danger"></p>
-                            </div>
-                            <div class="control-group">
-                                <input type="text" class="form-control" id="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
-                                <p class="help-block text-danger"></p>
-                            </div>
-                            <div class="control-group">
-                                <textarea class="form-control" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
-                                <p class="help-block text-danger"></p>
-                            </div>
-                            <div>
-                                <button class="btn btn-custom" type="submit" id="sendMessageButton">Send Message</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.4235573638407!2d113.72099831436081!3d-8.160010184012863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd695b617d8f623%3A0xf6c4437632474338!2sPoliteknik%20Negeri%20Jember!5e0!3m2!1sid!2sid!4v1667792765827!5m2!1sid!2sid"
-                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-            </div>
-        </div>
-        <!-- Contact End -->
+// Configuration option.
+// i.e. The standard subject will appear as, "You've been contacted by John Doe."
+
+// Example, $e_subject = '$name . ' has contacted you via Your Website.';
+
+$e_subject = 'You\'ve been contacted by ' . $first_name . '.';
 
 
-        <!-- Footer Start -->
-        <div class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="footer-contact">
-                            <h2>Get In Touch</h2>
-                            <p><i class="fa fa-map-marker-alt"></i>Jl. Mastrip PO BOX 164, Jember - Jawa Timur- Indonesia</p>
-                            <p><i class="fa fa-phone-alt"></i>+62 331 333533</p>
-                            <p><i class="fa fa-envelope"></i>politeknik@polije.ac.id</p>
-                            <div class="footer-social">
-                                <a class="btn" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn" href=""><i class="fab fa-youtube"></i></a>
-                                <a class="btn" href=""><i class="fab fa-instagram"></i></a>
-                                <a class="btn" href=""><i class="fab fa-linkedin-in"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="footer-link">
-                            <h2>Popular Links</h2>
-                            <a href="">About Us</a>
-                            <a href="">Contact Us</a>
-                            <a href="">Our Service</a>
-                            <a href="">Service Points</a>
-                            <a href="">Pricing Plan</a>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="footer-link">
-                            <h2>Useful Links</h2>
-                            <a href="">Terms of use</a>
-                            <a href="">Privacy policy</a>
-                            <a href="">Help</a>
-                            <a href="">FQAs</a>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="footer-newsletter">
-                            <h2>Newsletter</h2>
-                            <form>
-                                <input class="form-control" placeholder="Full Name">
-                                <input class="form-control" placeholder="Email">
-                                <button class="btn btn-custom">Submit</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="container copyright">
-                <p>&copy; <a href="index.html">J'Kost</a>, All Right Reserved.</p>
-            </div>
-        </div>
-        <!-- Footer End -->
+// Configuration option.
+// You can change this if you feel that you need to.
+// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
 
-        <!-- Back to top button -->
-        <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+$e_body = "You have been contacted by $first_name. $first_name selected service of $select_service, their additional message is as follows. Customer max budge is $select_price, for this project." . PHP_EOL . PHP_EOL;
+$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
+$e_reply = "You can contact $first_name via email, $email or via phone $phone";
 
-        <!-- Pre Loader -->
-        <div id="loader" class="show">
-            <div class="loader"></div>
-        </div>
+$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
 
-        <!-- JavaScript Libraries -->
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-        <script src="lib/easing/easing.min.js"></script>
-        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-        <script src="lib/waypoints/waypoints.min.js"></script>
-        <script src="lib/counterup/counterup.min.js"></script>
+$headers = "From: $email" . PHP_EOL;
+$headers .= "Reply-To: $email" . PHP_EOL;
+$headers .= "MIME-Version: 1.0" . PHP_EOL;
+$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
+$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
-        <!-- Contact Javascript File -->
-        <script src="mail/jqBootstrapValidation.min.js"></script>
-        <script src="mail/contact.js"></script>
+if(mail($address, $e_subject, $msg, $headers)) {
 
-        <!-- Template Javascript -->
-        <script src="js/main.js"></script>
-</body>
+	// Email has sent successfully, echo a success page.
 
-</html>
+	echo "<fieldset>";
+	echo "<div id='success_page'>";
+	echo "<h1>Email Sent Successfully.</h1>";
+	echo "<p>Thank you <strong>$first_name</strong>, your message has been submitted to us.</p>";
+	echo "</div>";
+	echo "</fieldset>";
+
+} else {
+
+	echo 'ERROR!';
+
+}
