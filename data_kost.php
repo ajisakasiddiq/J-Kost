ab84d1789f3f54d131ed719b7b10726924ef2e40
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2022-11-30 20:24:47 +0700

    update

--- data_kost.php
+++ data_kost.php
@@ -217,12 +217,19 @@ $sesEmail = $_SESSION['user_email'];
         </script>
         <!-- custom js -->
         <script src="js/custom.js"></script>
-        <script src="https://unpkg.com/bootstrap-table@1.21.0/dist/bootstrap-table.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
         <script>
             $(document).ready(function () {
                 $('#datakost').DataTable();
             });
-        </script></body>
+        </script>
+        </body>
 
 </html>
+
+
+<?php 
+
+
+?>
