861b1fa799fe32dd3edd646e1e85d77a3b9e3ba0
zidan <zidanprasetyo536@gmail.com>
2022-12-02 19:45:33 +0700

    Update profile_admin.php

--- profile_admin.php
+++ profile_admin.php
@@ -150,7 +150,7 @@ $sesEmail = $_SESSION['user_email'];
                                                                        </div>
                                                                        <form class="user" action="" method="POST">
                                                                        <div class="modal-body">
-                                                                       <input type="text" class="form-control form-control-user" id="exampleInputName"
+                                                                       <input type="hidden" class="form-control form-control-user" id="exampleInputName"
                                                                                 placeholder="Name" name="txt_id" value="<?= $user->id_user; ?>">
                                                                        <div class="form-group">
                                                                             <input type="text" class="form-control form-control-user" id="exampleInputName"
