8f867c8d0c7458337b04317b8ab423f8c0812248
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2022-12-03 18:32:17 +0700

    update

--- tambah_kost.php
+++ tambah_kost.php
@@ -55,13 +55,14 @@ if (isset($_SESSION['id_user'])) {
     <link rel="stylesheet" href="css/jquery.fancybox.css" />
     <!-- font awesome -->
     <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.css">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
-<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+
     <link href="https://unpkg.com/bootstrap-table@1.21.0/dist/bootstrap-table.min.css" rel="stylesheet">
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
       <![endif]-->
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
 </head>
 
 <body class="inner_page tables_page">
@@ -159,69 +160,84 @@ if (isset($_SESSION['id_user'])) {
         <!-- End Input Remark -->
 
         <!-- get location -->
-        <input type="text" class="form-control" id="Latitude" name="latitude" placeholder="latitude" required />
-        <input type="text" class="form-control" id="Longitude" name="longitude" placeholder="longitude" required />
-        <div class="mb-5" id="mapid" style="width: 1000px; height: 400px;"></div>
+        <input class="form-control" id="Latitude" name="Latitude" placeholder="latitude" required />
+        <input class="form-control" id="Longitude" name="Longitude" placeholder="longitude" required />
+        <div class="mt-2 mb-2" id="mapid" style="width: 1000px; height: 400px;"></div>
         <script>
-            const map = L.map('mapid').setView([-8.231935485535336, 113.60678852931734], 13);
+            const mymap = L.map('mapid').setView([-8.231935485535336, 113.60678852931734], 13);
             const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
-            }).addTo(map);
+            }).addTo(mymap);
 
-            var Latinput = document.querySelector("[name=latitude]");
-            var Lnginput = document.querySelector("[name=longitude]");
+            var Latinput = document.querySelector("[name=Latitude]");
+            var Lnginput = document.querySelector("[name=Longitude]");
+            
             var curLocation = [-8.231935485535336, 113.60678852931734];
             mymap.attributionControl.setPrefix(false);
+
             var marker = new L.marker(curLocation,{
-                draggable: 'true'
+                draggable: 'true',
             });
 
             marker.on('dragend', function(event){
                 var position = marker.getLatLng();
                 marker.setLatLng(position,{
-                    draggable: 'true'
+                    draggable: 'true',
                 }).bindPopup(position).update();
-                $("#Latitude").val(position.Lat);
-                $("#Longitude").val(position.Lng).keyup();
+                $("#Latitude").val(position.lat);
+                $("#Longitude").val(position.lng);
             });
 
             mymap.addLayer(marker);
+            
+
+            mymap.on("click", function(e){
+              var lat = e.latlng.lat;
+              var lng = e.latlng.lng;
+              if(!marker){
+                marker = L.marker(e.latlng).addTo(mymap);
+              }else{
+                marker.setLatLng(e.latlng);
+              }
+              Latinput.value = lat;
+              Lnginput.value = lng;
+            });
         </script>
 
         <!-- Start Submit Button -->
         <button class="btn btn-primary btn-block col-lg-2 ms-auto" type="submit">Submit</button>
         <!-- End Submit Button -->
       </form>
-                                </div>
-                            </div>
-                            <!-- jQuery -->
-                            <script src="js/jquery.min.js"></script>
-                            <script src="js/popper.min.js"></script>
-                            <script src="js/bootstrap.min.js"></script>
-                            <!-- wow animation -->
-                            <script src="js/animate.js"></script>
-                            <!-- select country -->
-                            <script src="js/bootstrap-select.js"></script>
-                            <!-- owl carousel -->
-                            <script src="js/owl.carousel.js"></script>
-                            <!-- chart js -->
-                            <script src="js/Chart.min.js"></script>
-                            <script src="js/Chart.bundle.min.js"></script>
-                            <script src="js/utils.js"></script>
-                            <script src="js/analyser.js"></script>
-                            <!-- nice scrollbar -->
-                            <script src="js/perfect-scrollbar.min.js"></script>
-                            <script>
-                                var ps = new PerfectScrollbar('#sidebar');
-                            </script>
-                            <!-- fancy box js -->
-                            <script src="js/jquery-3.3.1.min.js"></script>
-                            <script src="js/jquery.fancybox.min.js"></script>
-                            <!-- custom js -->
-                            <script src="js/custom.js"></script>
-                            <!-- calendar file css -->
-                            <script src="js/semantic.min.js"></script>
-                            <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+    </div>
+</div>
+<!-- jQuery -->
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- wow animation -->
+<script src="js/animate.js"></script>
+<!-- select country -->
+<script src="js/bootstrap-select.js"></script>
+<!-- owl carousel -->
+<script src="js/owl.carousel.js"></script>
+<!-- chart js -->
+<script src="js/Chart.min.js"></script>
+<script src="js/Chart.bundle.min.js"></script>
+<script src="js/utils.js"></script>
+<script src="js/analyser.js"></script>
+<!-- nice scrollbar -->
+<script src="js/perfect-scrollbar.min.js"></script>
+<script>
+    var ps = new PerfectScrollbar('#sidebar');
+</script>
+<!-- fancy box js -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery.fancybox.min.js"></script>
+<!-- custom js -->
+<script src="js/custom.js"></script>
+<!-- calendar file css -->
+<script src="js/semantic.min.js"></script>
+                           
 </body>
 
 </html>
