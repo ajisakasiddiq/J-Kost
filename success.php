8b9638ae5617ba104f9cdfdbda0f124bef227c44
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2022-12-07 16:55:50 +0700

    up

---
+++ success.php
@@ -0,0 +1,106 @@
+<?php
+require("koneksi.php");
+
+session_start();
+
+if (isset($_SESSION['id_user'])) {
+    //$_SESSION['msg'] = 'anda harus login untuk mengakses halaman ini';
+   // header('Location: login.php');
+   $sesID = $_SESSION['id_user'];
+   $sesName = $_SESSION['username'];
+   $name = $_SESSION['user_nama'];
+   $sesEmail = $_SESSION['user_email'];
+   $sesLvl = $_SESSION['level'];
+   $sesImg = $_SESSION['foto'];
+   $sesNik = $_SESSION['nik'];
+   $sesAddress = $_SESSION['alamat'];
+   $sesNo = $_SESSION['no_hp'];
+   $sesGender = $_SESSION['jenis_kelamin'];
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>J-KOS</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free Website Template" name="keywords">
+    <meta content="Free Website Template" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <!-- CSS Libraries -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="page-content page-success">
+      <div class="section-success" data-aos="zoom-in">
+        <div class="container">
+          <div class="row align-items-center row-login justify-content-center">
+            <div class="col-lg-6 text-center">
+              <img src="img/logo-kost.png" alt="" class="mb-4" />
+              <h2>
+                Transaction Processed!
+              </h2>
+              <p>
+                Silahkan tunggu konfirmasi email dari kami dan <br />
+                kami akan menginformasikan resi secept mungkin!
+              </p>
+              <div>
+                <a class="btn btn-custom w-50 mt-4" href="dashboard">
+                  My Dashboard
+                </a>
+                <a class="btn btn-signup w-50 mt-2" href="order">
+                  Go To Shopping
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <p class="pt-4 pb-2">
+              2019 Copyright J'Kost. All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
